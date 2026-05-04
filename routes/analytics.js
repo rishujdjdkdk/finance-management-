381fc91abf53e4d6bd2f8b4fee7c496bfe8eec04
rishujdjdkdk <rishuyadav2372004@gmail.com>
2026-05-04 08:44:46 +0530

    Delete routes/analytics.js

--- routes/analytics.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-
-const { getAnalytics } = require('../controllers/analyticsController');
-
-router.get('/', getAnalytics);
-
-module.exports = router;
